aedeac93a86961ee2f4f54146d8b580eb40cccf4
danischm <danischm@cisco.com>
2025-08-05 18:44:34 +0200

    Update msdp model

--- iosxe_msdp.tf
+++ iosxe_msdp.tf
@@ -1,44 +1,49 @@
-locals {
-  msdp_passwords = flatten([
-    for device in local.devices : [
-      for password in try(local.device_config[device.name].msdp.passwords, []) : {
-        key    = format("%s/%s", device.name, password.addr)
-        device = device.name
+resource "iosxe_msdp" "msdp" {
+  for_each = { for device in local.devices : device.name => device if(try(local.device_config[device.name].msdp, null) != null || try(local.defaults.iosxe.configuration.msdp, null) != null) && try(local.device_config[device.name].msdp.vrf, null) == null || (try(local.defaults.iosxe.configuration.msdp.vrf, null) == null) }
+  device   = each.value.name
 
-        addr       = try(password.addr, local.defaults.iosxe.configuration.msdp.passwords.addr, null)
-        encryption = try(password.encryption, local.defaults.iosxe.configuration.msdp.passwords.encryption, null)
-        password   = try(password.password, local.defaults.iosxe.configuration.msdp.passwords.password, null)
-    }]
-  ])
-
-  msdp_peers = flatten([
-    for device in local.devices : [
-      for peer in try(local.device_config[device.name].msdp.peers, []) : {
-        key    = format("%s/%s", device.name, peer.addr)
-        device = device.name
-
-        addr                    = try(peer.addr, local.defaults.iosxe.configuration.msdp.peers.addr, null)
-        remote_as               = try(peer.remote_as, local.defaults.iosxe.configuration.msdp.peers.remote_as, null)
-        connect_source_loopback = try(peer.connect_source_loopback, local.defaults.iosxe.configuration.msdp.peers.connect_source_loopback, null)
-    }]
-  ])
+  originator_id = try(local.device_config[each.value.name].msdp.originator_id, local.defaults.iosxe.configuration.msdp.originator_id, null)
+  passwords = [for password in try(local.device_config[each.value.name].msdp.passwords, []) : {
+    addr       = try(password.host, local.defaults.iosxe.configuration.msdp.passwords.host, null)
+    encryption = try(password.encryption, local.defaults.iosxe.configuration.msdp.passwords.encryption, null)
+    password   = try(password.password, local.defaults.iosxe.configuration.msdp.passwords.password, null)
+  }]
+  peers = [for peer in try(local.device_config[each.value.name].msdp.peers, []) : {
+    addr                    = try(peer.host, local.defaults.iosxe.configuration.msdp.peers.host, null)
+    remote_as               = try(peer.remote_as, local.defaults.iosxe.configuration.msdp.peers.remote_as, null)
+    connect_source_loopback = try(peer.connect_source_interface_type, local.defaults.iosxe.configuration.msdp.peers.connect_source_interface_type, null) == "Loopback" ? try(peer.connect_source_interface_id, local.defaults.iosxe.configuration.msdp.peers.connect_source_interface_id, null) : null
+  }]
 }
 
-resource "iosxe_msdp" "msdp" {
-
-  for_each      = { for device in local.devices : device.name => device if(try(local.device_config[device.name].msdp, null) != null || try(local.defaults.iosxe.configuration.msdp, null) != null) && try(local.device_config[device.name].msdp.vrf, null) == null || (try(local.defaults.iosxe.configuration.msdp.vrf, null) == null) }
-  device        = each.value.name
-  originator_id = try(local.device_config[each.value.name].msdp.originator_id, local.defaults.iosxe.configuration.msdp.originator_id, null)
-  passwords     = length(local.msdp_passwords) > 0 ? local.msdp_passwords : null
-  peers         = length(local.msdp_peers) > 0 ? local.msdp_peers : null
+locals {
+  msdp_vrfs = flatten([
+    for device in local.devices : [
+      for vrf in try(local.device_config[device.name].msdp.vrfs, []) : {
+        key           = format("%s/%s", device.name, vrf.vrf)
+        device        = device.name
+        vrf           = try(vrf.vrf, null)
+        originator_id = try(local.device_config[device.name].msdp.originator_id, local.defaults.iosxe.configuration.msdp.originator_id, null)
+        passwords = [for password in try(local.device_config[device.name].msdp.passwords, []) : {
+          addr       = try(password.host, local.defaults.iosxe.configuration.msdp.passwords.host, null)
+          encryption = try(password.encryption, local.defaults.iosxe.configuration.msdp.passwords.encryption, null)
+          password   = try(password.password, local.defaults.iosxe.configuration.msdp.passwords.password, null)
+        }]
+        peers = [for peer in try(local.device_config[device.name].msdp.peers, []) : {
+          addr                    = try(peer.host, local.defaults.iosxe.configuration.msdp.peers.host, null)
+          remote_as               = try(peer.remote_as, local.defaults.iosxe.configuration.msdp.peers.remote_as, null)
+          connect_source_loopback = try(peer.connect_source_interface_type, local.defaults.iosxe.configuration.msdp.peers.connect_source_interface_type, null) == "Loopback" ? try(peer.connect_source_interface_id, local.defaults.iosxe.configuration.msdp.peers.connect_source_interface_id, null) : null
+        }]
+      }
+    ]
+  ])
 }
 
 resource "iosxe_msdp_vrf" "msdp_vrf" {
-  for_each = { for device in local.devices : device.name => device if(try(local.device_config[device.name].msdp, null) != null || try(local.defaults.iosxe.configuration.msdp, null) != null) && try(local.device_config[device.name].msdp.vrf, null) != null || (try(local.defaults.iosxe.configuration.msdp.vrf, null) != null) }
-  device   = each.value.name
+  for_each = { for e in local.msdp_vrfs : e.key => e }
+  device   = each.value.device
 
-  vrf           = local.device_config[each.value.name].msdp.vrf
-  originator_id = try(local.device_config[each.value.name].msdp.originator_id, local.defaults.iosxe.configuration.msdp.originator_id, null)
-  passwords     = length(local.msdp_passwords) > 0 ? local.msdp_passwords : null
-  peers         = length(local.msdp_peers) > 0 ? local.msdp_peers : null
+  vrf           = each.value.vrf
+  originator_id = each.value.originator_id
+  passwords     = each.value.passwords
+  peers         = each.value.peers
 }
