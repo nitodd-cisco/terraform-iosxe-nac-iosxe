03aaa27c470207e27962ba6ff1a8a1fa3eac7b1f
danischm <danischm@cisco.com>
2025-08-16 14:58:52 +0200

    Bfd model updates and fixes

--- iosxe_bfd.tf
+++ iosxe_bfd.tf
@@ -27,7 +27,7 @@ resource "iosxe_bfd" "bfd" {
     dst_vrf       = try(e.destination_vrf, local.defaults.iosxe.configuration.bfd.ipv4_maps.destination_vrf, null)
     dest_ip       = try(e.destination_prefix, local.defaults.iosxe.configuration.bfd.ipv4_maps.destination_prefix, null)
     src_ip        = try(e.source_prefix, local.defaults.iosxe.configuration.bfd.ipv4_maps.source_prefix, null)
-    template_name = e.template_name
+    template_name = e.template
   }]
 
   ipv6_with_both_vrfs = [for e in try(local.device_config[each.value.name].bfd.ipv6_maps, []) : {
@@ -39,8 +39,8 @@ resource "iosxe_bfd" "bfd" {
   } if try(e.source_vrf, null) != null && try(e.destination_vrf, null) != null]
 
   ipv6_without_vrfs = [for e in try(local.device_config[each.value.name].bfd.ipv6_maps, []) : {
-    dest_ip       = try(e.destination_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.destination_prefix, null)
-    src_ip        = try(e.source_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.source_prefix, null)
+    dest_ipv6     = try(e.destination_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.destination_prefix, null)
+    src_ipv6      = try(e.source_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.source_prefix, null)
     template_name = e.template
   } if try(e.source_vrf, null) == null && try(e.destination_vrf, null) == null]
 
@@ -53,9 +53,9 @@ resource "iosxe_bfd" "bfd" {
 
   ipv6_with_dst_vrfs = [for e in try(local.device_config[each.value.name].bfd.ipv6_maps, []) : {
     dst_vrf       = try(e.destination_vrf, local.defaults.iosxe.configuration.bfd.ipv6_maps.destination_vrf, null)
-    dest_ip       = try(e.destination_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.destination_prefix, null)
-    src_ip        = try(e.source_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.source_prefix, null)
-    template_name = e.template_name
+    dest_ipv6     = try(e.destination_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.destination_prefix, null)
+    src_ipv6      = try(e.source_prefix, local.defaults.iosxe.configuration.bfd.ipv6_maps.source_prefix, null)
+    template_name = e.template
   }]
 
   slow_timers = try(local.device_config[each.value.name].bfd.slow_timers, local.defaults.iosxe.configuration.bfd.slow_timers, null)
@@ -70,8 +70,8 @@ locals {
         name                                     = try(template.name, local.defaults.iosxe.configuration.bfd.single_hop_templates.name, null)
         authentication_md5_keychain              = try(template.authentication_md5_keychain, local.defaults.iosxe.configuration.bfd.single_hop_templates.authentication_md5_keychain, null)
         authentication_meticulous_md5_keychain   = try(template.authentication_meticulous_md5_keychain, local.defaults.iosxe.configuration.bfd.single_hop_templates.authentication_meticulous_md5_keychain, null)
-        authentication_meticulous_sha_1_keychain = try(template.authentication_meticulous_sha_1_keychain, local.defaults.iosxe.configuration.bfd.single_hop_templates.authentication_meticulous_sha_1_keychain, null)
-        authentication_sha_1_keychain            = try(template.authentication_sha_1_keychain, local.defaults.iosxe.configuration.bfd.single_hop_templates.authentication_sha_1_keychain, null)
+        authentication_meticulous_sha_1_keychain = try(template.authentication_meticulous_sha1_keychain, local.defaults.iosxe.configuration.bfd.single_hop_templates.authentication_meticulous_sha1_keychain, null)
+        authentication_sha_1_keychain            = try(template.authentication_sha1_keychain, local.defaults.iosxe.configuration.bfd.single_hop_templates.authentication_sha1_keychain, null)
         interval_milliseconds_min_tx             = try(template.interval_milliseconds_min_tx, local.defaults.iosxe.configuration.bfd.single_hop_templates.interval_milliseconds_min_tx, null)
         interval_milliseconds_min_rx             = try(template.interval_milliseconds_min_rx, local.defaults.iosxe.configuration.bfd.single_hop_templates.interval_milliseconds_min_rx, null)
         interval_milliseconds_both               = try(template.interval_milliseconds_both, local.defaults.iosxe.configuration.bfd.single_hop_templates.interval_milliseconds_both, null)
@@ -120,7 +120,7 @@ locals {
         authentication_md5_keychain             = try(template.authentication_md5_keychain, local.defaults.iosxe.configuration.bfd.multi_hop_templates.authentication_md5_keychain, null)
         authentication_meticulous_md5_keychain  = try(template.authentication_meticulous_md5_keychain, local.defaults.iosxe.configuration.bfd.multi_hop_templates.authentication_meticulous_md5_keychain, null)
         authentication_meticulous_sha_1keychain = try(template.authentication_meticulous_sha1_keychain, local.defaults.iosxe.configuration.bfd.multi_hop_templates.authentication_meticulous_sha1_keychain, null)
-        authentication_sha_1_keychain           = try(template.authentication_sha_1_keychain, local.defaults.iosxe.configuration.bfd.multi_hop_templates.authentication_sha_1_keychain, null)
+        authentication_sha_1_keychain           = try(template.authentication_sha1_keychain, local.defaults.iosxe.configuration.bfd.multi_hop_templates.authentication_sha1_keychain, null)
         interval_milliseconds_both              = try(template.interval_milliseconds_both, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_milliseconds_both, null)
         interval_milliseconds_min_tx            = try(template.interval_milliseconds_min_tx, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_milliseconds_min_tx, null)
         interval_milliseconds_min_rx            = try(template.interval_milliseconds_min_rx, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_milliseconds_min_rx, null)
@@ -129,6 +129,7 @@ locals {
         interval_microseconds_min_tx            = try(template.interval_microseconds_min_tx, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_microseconds_min_tx, null)
         interval_microseconds_min_rx            = try(template.interval_microseconds_min_rx, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_microseconds_min_rx, null)
         interval_microseconds_multiplier        = try(template.interval_microseconds_multiplier, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_microseconds_multiplier, null)
+        interval_microseconds                   = try(template.interval_microseconds, local.defaults.iosxe.configuration.bfd.multi_hop_templates.interval_microseconds, null)
         echo                                    = try(template.echo, local.defaults.iosxe.configuration.bfd.multi_hop_templates.echo, null)
         dampening_half_time                     = try(template.dampening_half_time, local.defaults.iosxe.configuration.bfd.multi_hop_templates.dampening_half_time, null)
         dampening_unsuppress_time               = try(template.dampening_unsuppress_time, local.defaults.iosxe.configuration.bfd.multi_hop_templates.dampening_unsuppress_time, null)
@@ -158,6 +159,7 @@ resource "iosxe_bfd_template_multi_hop" "bfd_template_multi_hop" {
   interval_microseconds_min_tx            = each.value.interval_microseconds_min_tx
   interval_microseconds_min_rx            = each.value.interval_microseconds_min_rx
   interval_microseconds_multiplier        = each.value.interval_microseconds_multiplier
+  interval_microseconds                   = each.value.interval_microseconds
   echo                                    = each.value.echo
   dampening_half_time                     = each.value.dampening_half_time
   dampening_unsuppress_time               = each.value.dampening_unsuppress_time
