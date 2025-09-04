0f9fd14c36f6994e6fbb1bcd9ac8c06b9aca6725
danischm <danischm@cisco.com>
2025-09-04 13:30:04 +0200

    Line model updates

--- iosxe_line.tf
+++ iosxe_line.tf
@@ -3,16 +3,23 @@ resource "iosxe_line" "line" {
   device   = each.value.name
 
   console = try(length(local.device_config[each.value.name].line.consoles) == 0, true) ? null : [for c in local.device_config[each.value.name].line.consoles : {
-    first                = "0"
-    exec_timeout_minutes = try(c.exec_timeout_minutes, local.defaults.iosxe.configuration.line.consoles.exec_timeout_minutes, null)
-    exec_timeout_seconds = try(c.exec_timeout_seconds, local.defaults.iosxe.configuration.line.consoles.exec_timeout_seconds, null)
-    login_authentication = try(c.login_authentication, local.defaults.iosxe.configuration.line.consoles.login_authentication, null)
-    login_local          = try(c.login_local, local.defaults.iosxe.configuration.line.consoles.login_local, null)
-    password             = try(c.password, local.defaults.iosxe.configuration.line.consoles.password, null)
-    password_level       = try(c.password_level, local.defaults.iosxe.configuration.line.consoles.password_level, null)
-    password_type        = try(c.password_type, local.defaults.iosxe.configuration.line.consoles.password_type, null)
-    privilege_level      = try(c.privilege_level, local.defaults.iosxe.configuration.line.consoles.privilege_level, null)
-    stopbits             = try(c.stopbits, local.defaults.iosxe.configuration.line.consoles.stopbits, null)
+    first                 = try(c.number, local.defaults.iosxe.configuration.line.consoles.number, "0")
+    exec_timeout_minutes  = try(c.exec_timeout_minutes, local.defaults.iosxe.configuration.line.consoles.exec_timeout_minutes, null)
+    exec_timeout_seconds  = try(c.exec_timeout_seconds, local.defaults.iosxe.configuration.line.consoles.exec_timeout_seconds, null)
+    login_authentication  = try(c.login_authentication, local.defaults.iosxe.configuration.line.consoles.login_authentication, null)
+    login_local           = try(c.login_local, local.defaults.iosxe.configuration.line.consoles.login_local, null)
+    password              = try(c.password, local.defaults.iosxe.configuration.line.consoles.password, null)
+    password_level        = try(c.password_level, local.defaults.iosxe.configuration.line.consoles.password_level, null)
+    password_type         = try(c.password_type, local.defaults.iosxe.configuration.line.consoles.password_type, null)
+    privilege_level       = try(c.privilege_level, local.defaults.iosxe.configuration.line.consoles.privilege_level, null)
+    stopbits              = try(c.stopbits, local.defaults.iosxe.configuration.line.consoles.stopbits, null)
+    session_timeout       = try(c.session_timeout, local.defaults.iosxe.configuration.line.consoles.session_timeout, null)
+    monitor               = try(c.monitor, local.defaults.iosxe.configuration.line.consoles.monitor, null)
+    escape_character      = try(c.escape_character, local.defaults.iosxe.configuration.line.consoles.escape_character, null)
+    logging_synchronous   = try(c.logging_synchronous, local.defaults.iosxe.configuration.line.consoles.logging_synchronous, null)
+    transport_output_all  = contains(try(c.transport_output, local.defaults.iosxe.configuration.line.consoles.transport_output, []), "all") ? true : null
+    transport_output_none = contains(try(c.transport_output, local.defaults.iosxe.configuration.line.consoles.transport_output, []), "none") ? true : null
+    transport_output      = length(setsubtract(try(c.transport_output, local.defaults.iosxe.configuration.line.consoles.transport_output, []), ["all", "none"])) > 0 ? setsubtract(try(c.transport_output, local.defaults.iosxe.configuration.line.consoles.transport_output, []), ["all", "none"])[0] : null
   }]
 
   vty = try(length(local.device_config[each.value.name].line.vtys) == 0, true) ? null : [for v in local.device_config[each.value.name].line.vtys : {
@@ -36,6 +43,22 @@ resource "iosxe_line" "line" {
     transport_input_all          = try(v.transport_input_all, local.defaults.iosxe.configuration.line.vtys.transport_input_all, null)
     transport_input_none         = try(v.transport_input_none, local.defaults.iosxe.configuration.line.vtys.transport_input_none, null)
     transport_input              = try(v.transport_input, local.defaults.iosxe.configuration.line.vtys.transport_input, null)
+    session_timeout              = try(v.session_timeout, local.defaults.iosxe.configuration.line.vtys.session_timeout, null)
+    monitor                      = try(v.monitor, local.defaults.iosxe.configuration.line.vtys.monitor, null)
+    logging_synchronous          = try(v.logging_synchronous, local.defaults.iosxe.configuration.line.vtys.logging_synchronous, null)
+    transport_output_all         = contains(try(v.transport_output, local.defaults.iosxe.configuration.line.vtys.transport_output, []), "all") ? true : null
+    transport_output_none        = contains(try(v.transport_output, local.defaults.iosxe.configuration.line.vtys.transport_output, []), "none") ? true : null
+    transport_output             = length(setsubtract(try(v.transport_output, local.defaults.iosxe.configuration.line.vtys.transport_output, []), ["all", "none"])) > 0 ? setsubtract(try(v.transport_output, local.defaults.iosxe.configuration.line.vtys.transport_output, []), ["all", "none"])[0] : null
+  }]
+
+  aux = try(length(local.device_config[each.value.name].line.auxes) == 0, true) ? null : [for a in local.device_config[each.value.name].line.auxes : {
+    first                 = try(a.number, local.defaults.iosxe.configuration.line.auxes.number, null)
+    exec_timeout_minutes  = try(a.exec_timeout_minutes, local.defaults.iosxe.configuration.line.auxes.exec_timeout_minutes, null)
+    exec_timeout_seconds  = try(a.exec_timeout_seconds, local.defaults.iosxe.configuration.line.auxes.exec_timeout_seconds, null)
+    monitor               = try(a.monitor, local.defaults.iosxe.configuration.line.auxes.monitor, null)
+    escape_character      = try(a.escape_character, local.defaults.iosxe.configuration.line.auxes.escape_character, null)
+    logging_synchronous   = try(a.logging_synchronous, local.defaults.iosxe.configuration.line.auxes.logging_synchronous, null)
+    transport_output_none = contains(try(a.transport_output, local.defaults.iosxe.configuration.line.auxes.transport_output, []), "none") ? true : null
   }]
 
   depends_on = [
