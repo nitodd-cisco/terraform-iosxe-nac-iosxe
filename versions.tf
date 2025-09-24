72812e2cc75bcbb937cba4212da2c29e8094548d
danischm <danischm@cisco.com>
2025-09-24 18:35:30 +0200

    Bump provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     iosxe = {
       source  = "CiscoDevNet/iosxe"
-      version = "= 0.8.1"
+      version = "= 0.9.0"
     }
     utils = {
       source  = "netascode/utils"
